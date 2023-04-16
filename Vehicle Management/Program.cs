7bd2eabc75c04949f970bb83b925c39b6b3fbd21
Biraj <birajkmc4@gmail.com>
2023-04-16 17:19:27 +0545

    Changes in views. Scaffolding and in register page to accomodate changes.

--- Vehicle Management/Program.cs
+++ Vehicle Management/Program.cs
@@ -1,5 +1,4 @@
 global using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Vehicle_Management.Data;
 
@@ -9,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
