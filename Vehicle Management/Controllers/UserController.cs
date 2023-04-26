674b9ce0ebe110269866988acb4da3c5362f72c1
Biraj <birajkmc4@gmail.com>
2023-04-26 16:52:53 +0545

    User Request for Vehicle

--- Vehicle Management/Controllers/UserController.cs
+++ Vehicle Management/Controllers/UserController.cs
@@ -1,13 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Vehicle_Management.Models;
 
 namespace Vehicle_Management.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ILogger<HomeController> logger, ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Home()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Home(UserRequest model, Request newRequest, RequestMessage newRequestMessage, RequestStatus newRequestStatus)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            newRequestStatus.CreatedById = currentUser.Id;
+            newRequestStatus.CreatedDate = DateTime.Now;
+            _dbContext.Add(newRequestStatus);
+            _dbContext.SaveChanges();
+            int newRequestStatusId = newRequestStatus.Id;
+
+            newRequest.PickupPoint = model.PickupPoint;
+            newRequest.PickupPointLandmark = model.PickupPointLandmark;
+            newRequest.DropPoint = model.DropPoint;
+            newRequest.DropPointLandmark = model.DropPointLandmark;
+            newRequest.RequestedDate = model.RequestedDate;
+            newRequest.UserId = currentUser.Id;
+            newRequest.CreatedbyId = currentUser.Id;
+            newRequest.CreatedDate = DateTime.Now;
+            newRequest.RequestStatusId = newRequestStatusId;
+            _dbContext.Add(newRequest);
+            _dbContext.SaveChanges();
+            int newRequestId = newRequest.Id;
+
+            newRequestMessage.Message = model.Message;
+            newRequestMessage.CreatedDate = DateTime.Now;
+            newRequestMessage.RequestId = newRequestId;
+            newRequestMessage.CreatedById = currentUser.Id;
+            _dbContext.Add(newRequestMessage);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Home");
+
         }
     }
 }
--- Vehicle Management/Models/UserRequest.cs
+++ Vehicle Management/Models/UserRequest.cs
@@ -2,5 +2,22 @@
 {
     public class UserRequest
     {
+        public int Id { get; set; }
+        public DateTime RequestedDate { get; set; }
+        public string? PickupPoint { get; set; }
+        public string? PickupPointLandmark { get; set; }
+        public string? DropPoint { get; set; }
+        public string? DropPointLandmark { get; set; }
+        public bool IsApproved { get; set; } = false;
+        public bool IsCompleted { get; set; } = false;
+        public string? Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public int RequestStatusId { get; set; }
+        public RequestStatus? RequestStatus { get; set; }
+
+        public string? UserId { get; set; }
+        public string? CreatedbyId { get; set; }
+        public ApplicationUser? User { get; set; }
     }
 }
